adc37b7ecac80b74e328c7a3dd752ac05b670122
Luiz Felipe <luiz.felipe.professional@hotmail.com>
2022-05-18 21:04:58 -0300

    Executing commands unit testing

---
+++ Todo.Domain.Tests/Commands/CreateTodoCommandTests.cs
@@ -0,0 +1,27 @@
+﻿using Todo.Domain.Commands;
+
+namespace Todo.Domain.Tests.Commands;
+
+[TestClass]
+public class CreateTodoCommandTests
+{
+    private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.Now);
+    private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Task Title", "Luiz Felipe LP LP", DateTime.Now);
+
+    public CreateTodoCommandTests()
+    {
+        _invalidCommand.Validade();
+        _validCommand.Validade();
+    }
+
+    [TestMethod]
+    [TestCategory("Commands")]
+    public void ReturnFalseWhenCommandIsNotValid()
+        => Assert.AreEqual(false, _invalidCommand.IsValid);
+
+    [TestMethod]
+    [TestCategory("Commands")]
+    public void ReturnTrueWhenCommandIsValid()
+        => Assert.AreEqual(true, _validCommand.IsValid);
+}
+
