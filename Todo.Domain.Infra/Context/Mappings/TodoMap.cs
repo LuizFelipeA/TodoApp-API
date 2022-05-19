31dc85c6d60569182f4de36f47078e2daf7cd3d6
Luiz Felipe <luiz.felipe.professional@hotmail.com>
2022-05-19 11:55:45 -0300

    Creating context and mapping entities

---
+++ Todo.Domain.Infra/Context/Mappings/TodoMap.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Infra.Context.Mappings;
+
+public class TodoMap : IEntityTypeConfiguration<TodoItem>
+{
+    public void Configure(EntityTypeBuilder<TodoItem> builder)
+    {
+        // Table name
+        builder.ToTable("Todo");
+
+        // Primary Key
+        builder.HasKey(x => x.Id);
+
+        // Identity
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn();
+
+        // Properties
+        builder.Property(x => x.User)
+            .HasMaxLength(120)
+            .HasColumnType("VARCHAR(120)");
+
+        builder.Property(x => x.Title)
+            .IsRequired()
+            .HasMaxLength(160);
+
+        builder.Property(x => x.Done)
+            .HasColumnType("BIT");
+
+        builder.Property(x => x.Date);
+
+        // Indexes
+        builder.HasIndex(x => x.User)
+            .IsUnique();
+    }
+}
+
---
+++ Todo.Domain.Infra/Context/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Entities;
+using Todo.Domain.Infra.Context.Mappings;
+
+namespace Todo.Domain.Infra.Context;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options) { }
+
+    public DbSet<TodoItem> Todos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new TodoMap());
+    }
+}
+
